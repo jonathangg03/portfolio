e9aae50ad029841bb2a32280440ea9c25fb4da77
Jonathan García <jona03g97@gmail.com>
2021-10-27 16:39:25 -0600

    Finish intersection

--- intersections.js
+++ intersections.js
@@ -1,7 +1,11 @@
 const $hero = document.querySelector('.header__hero')
 const $headerElement = document.querySelector('.header')
+const $headerTop = document.querySelector('.header__top')
 const $aboutMeDescription = document.querySelector('.aboutme__description')
 const $aboutMeObjectives = document.querySelector('.aboutme__objectives')
+const $habiitiesContainer = document.querySelector('.habilities__container')
+const $habiitiesItems = document.querySelectorAll('.habilities__item')
+const $projectsItems = document.querySelectorAll('.projects__item')
 
 const intersections = () => {
   const loadElements = (enters) => {
@@ -9,15 +13,24 @@ const intersections = () => {
       if (enter.isIntersecting) {
         if (enter.target.className === 'header') {
           $hero.classList.add('moveHero')
+          $headerTop.classList.add('moveHeaderTop')
         }
 
-        if (enter.target.className === 'aboutme__description') {
+        if (
+          enter.target.className === 'aboutme__description' ||
+          enter.target.className === 'aboutme__objectives'
+        ) {
           enter.target.classList.add('moveAboutme')
         }
 
-        if (enter.target.className === 'aboutme__objectives') {
-          console.log('entro')
-          enter.target.classList.add('moveAboutme')
+        if (enter.target.className === 'habilities__container') {
+          $habiitiesItems.forEach((hability) => {
+            hability.classList.add('moveHability')
+          })
+        }
+
+        if (enter.target.className === 'projects__item') {
+          enter.target.classList.add('moveProject')
         }
       }
     })
@@ -31,6 +44,11 @@ const intersections = () => {
 
   observer.observe($headerElement)
   observer.observe($aboutMeDescription)
+  observer.observe($aboutMeObjectives)
+  observer.observe($habiitiesContainer)
+  $projectsItems.forEach((item) => {
+    observer.observe(item)
+  })
 }
 
 window.addEventListener('load', intersections)
